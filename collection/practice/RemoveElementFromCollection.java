a8cf6bc13d87cc0c722ca2a72f60671690b97864
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2018-09-23 21:56:36 +0530

    Add files via upload

---
+++ collection/practice/RemoveElementFromCollection.java
@@ -0,0 +1,36 @@
+package com.project.java.collection.api.practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/*
+ * Whenever it finds any structural modification in the underlying data structure (ex: ArrayList in this case)
+ * It will throw Runtime exception
+ * 
+ * */
+public class RemoveElementFromCollection {
+	/*
+	 * This will throw run time exception anyhow public void
+	 * removeFromCollection(List<Integer> marks) { for (Integer mark : marks) {
+	 * if (mark < 40) // Will throw (java.util.ConcurrentModificationException)
+	 * marks.remove(mark); } }
+	 */
+
+	public static void main(String[] args) {
+		RemoveElementFromCollection test = new RemoveElementFromCollection();
+		List<Integer> list = new ArrayList<Integer>(Arrays.asList(10, 20, 40, 50, 60));
+		test.removeFromCollection(list);
+		System.out.println(list);
+		
+	}
+
+	public void removeFromCollection(List<Integer> marks) {
+		for (Iterator<Integer> iterator = marks.iterator(); iterator.hasNext();) {
+			Integer mark = iterator.next();
+			if (mark < 40)
+				iterator.remove(); // ==> Safe to call remove() on Iterator
+		}
+	}
+}
