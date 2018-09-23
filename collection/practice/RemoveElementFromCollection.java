32e788cf21fc90ff501122993c25c6c63bdccc0a
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2018-09-23 22:02:25 +0530

    Add files via upload

--- collection/practice/RemoveElementFromCollection.java
+++ collection/practice/RemoveElementFromCollection.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
  * Whenever it finds any structural modification in the underlying data structure (ex: ArrayList in this case)
@@ -23,7 +25,8 @@ public class RemoveElementFromCollection {
 		List<Integer> list = new ArrayList<Integer>(Arrays.asList(10, 20, 40, 50, 60));
 		test.removeFromCollection(list);
 		System.out.println(list);
-		
+		//Java-8
+		test.removeFromCollection();
 	}
 
 	public void removeFromCollection(List<Integer> marks) {
@@ -33,4 +36,10 @@ public class RemoveElementFromCollection {
 				iterator.remove(); // ==> Safe to call remove() on Iterator
 		}
 	}
+	
+	public void removeFromCollection() {
+		List<Integer> collect1 = Stream.of(10, 20, 30, 40).collect(Collectors.toList());
+		collect1.removeIf(integer -> integer > 15);
+		collect1.forEach(System.out::println);
+		}
 }
